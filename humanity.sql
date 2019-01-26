db010398415b803649f9218b9c7160ebc296734c
Umar Farooq <umar.bwn@gmail.com>
2019-01-26 23:16:23 +0500

    Added updated database

--- humanity.sql
+++ humanity.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Jan 21, 2019 at 03:22 PM
--- Server version: 5.7.23
--- PHP Version: 5.6.38
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2019 at 07:15 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,23 +28,20 @@ SET time_zone = "+00:00";
 -- Table structure for table `clock_day_counter`
 --
 
-DROP TABLE IF EXISTS `clock_day_counter`;
-CREATE TABLE IF NOT EXISTS `clock_day_counter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clock_day_counter` (
+  `id` int(11) NOT NULL,
   `clock_index` int(11) NOT NULL,
   `clock_time` datetime DEFAULT NULL,
-  `emp_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `clock_index` (`clock_index`),
-  KEY `emp_id` (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  `emp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `clock_day_counter`
 --
 
 INSERT INTO `clock_day_counter` (`id`, `clock_index`, `clock_time`, `emp_id`) VALUES
-(11, 325, NULL, 63);
+(53, 346, '2019-01-25 00:00:10', 63),
+(54, 346, '2019-01-26 00:00:02', 63);
 
 -- --------------------------------------------------------
 
@@ -52,9 +49,8 @@ INSERT INTO `clock_day_counter` (`id`, `clock_index`, `clock_time`, `emp_id`) VA
 -- Table structure for table `employees`
 --
 
-DROP TABLE IF EXISTS `employees`;
-CREATE TABLE IF NOT EXISTS `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
   `email_addr` varchar(200) NOT NULL,
@@ -64,11 +60,8 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `role` varchar(20) NOT NULL,
   `position` int(11) NOT NULL,
   `payroll_card` int(11) DEFAULT NULL,
-  `total_pay` double NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_addr` (`email_addr`),
-  KEY `payroll_card` (`payroll_card`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
+  `total_pay` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employees`
@@ -85,15 +78,12 @@ INSERT INTO `employees` (`id`, `first_name`, `last_name`, `email_addr`, `phone_n
 -- Table structure for table `leaves`
 --
 
-DROP TABLE IF EXISTS `leaves`;
-CREATE TABLE IF NOT EXISTS `leaves` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `leaves` (
+  `id` int(11) NOT NULL,
   `paid_leaves` int(2) NOT NULL,
   `emp_id` int(11) NOT NULL,
-  `leave_condition` int(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `emp_id` (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `leave_condition` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `leaves`
@@ -109,22 +99,20 @@ INSERT INTO `leaves` (`id`, `paid_leaves`, `emp_id`, `leave_condition`) VALUES
 -- Table structure for table `payroll_cards`
 --
 
-DROP TABLE IF EXISTS `payroll_cards`;
-CREATE TABLE IF NOT EXISTS `payroll_cards` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `payroll_cards` (
+  `id` int(11) NOT NULL,
   `card_title` varchar(200) NOT NULL,
   `pay_rate` int(3) NOT NULL,
   `daily_hours` int(2) NOT NULL,
-  `hrly_rate_chart` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+  `hrly_rate_chart` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `payroll_cards`
 --
 
 INSERT INTO `payroll_cards` (`id`, `card_title`, `pay_rate`, `daily_hours`, `hrly_rate_chart`) VALUES
-(15, '8 Hours card', 100, 8, '{\"sun0\":\"100%\",\"mon0\":\"127%\",\"tue0\":\"100%\",\"wed0\":\"100%\",\"thu0\":\"127%\",\"fri0\":\"127%\",\"sat0\":\"100%\",\"sun0pm\":\"100%\",\"mon0pm\":\"100%\",\"tue0pm\":\"100%\",\"wed0pm\":\"100%\",\"thu0pm\":\"100%\",\"fri0pm\":\"100%\",\"sat0pm\":\"100%\",\"special_hours\":{\"_am_ip_mon0\":\"127%\",\"_am_ip_thu0\":\"127%\",\"_am_ip_fri0\":\"127%\",\"_am_ip_mon1\":\"127%\",\"_am_ip_thu1\":\"127%\",\"_am_ip_fri1\":\"127%\",\"_am_ip_mon2\":\"127%\",\"_am_ip_thu2\":\"127%\",\"_am_ip_fri2\":\"127%\",\"_am_ip_mon3\":\"127%\",\"_am_ip_thu3\":\"127%\",\"_am_ip_fri3\":\"127%\",\"_am_ip_mon4\":\"127%\",\"_am_ip_thu4\":\"127%\",\"_am_ip_fri4\":\"127%\",\"_am_ip_mon5\":\"127%\",\"_am_ip_thu5\":\"127%\",\"_am_ip_fri5\":\"127%\",\"_am_ip_mon6\":\"127%\",\"_am_ip_thu6\":\"127%\",\"_am_ip_fri6\":\"127%\",\"_am_ip_mon7\":\"127%\",\"_am_ip_thu7\":\"127%\",\"_am_ip_fri7\":\"127%\",\"_am_ip_mon8\":\"127%\",\"_am_ip_thu8\":\"127%\",\"_am_ip_fri8\":\"127%\",\"_am_ip_mon9\":\"127%\",\"_am_ip_thu9\":\"127%\",\"_am_ip_fri9\":\"127%\",\"_am_ip_mon10\":\"127%\",\"_am_ip_thu10\":\"127%\",\"_am_ip_fri10\":\"127%\",\"_am_ip_mon11\":\"127%\",\"_am_ip_thu11\":\"127%\",\"_am_ip_fri11\":\"127%\"}}');
+(15, '8 Hours card', 100, 8, '{\"sun0\":\"127%\",\"mon0\":\"127%\",\"tue0\":\"127%\",\"wed0\":\"127%\",\"thu0\":\"127%\",\"fri0\":\"127%\",\"sat0\":\"127%\",\"sun0pm\":\"100%\",\"mon0pm\":\"100%\",\"tue0pm\":\"100%\",\"wed0pm\":\"100%\",\"thu0pm\":\"100%\",\"fri0pm\":\"100%\",\"sat0pm\":\"100%\",\"special_hours\":{\"_am_ip_sun0\":\"127%\",\"_am_ip_mon0\":\"127%\",\"_am_ip_tue0\":\"127%\",\"_am_ip_wed0\":\"127%\",\"_am_ip_thu0\":\"127%\",\"_am_ip_fri0\":\"127%\",\"_am_ip_sat0\":\"127%\",\"_am_ip_sun1\":\"127%\",\"_am_ip_mon1\":\"127%\",\"_am_ip_tue1\":\"127%\",\"_am_ip_wed1\":\"127%\",\"_am_ip_thu1\":\"127%\",\"_am_ip_fri1\":\"127%\",\"_am_ip_sat1\":\"127%\",\"_am_ip_sun2\":\"127%\",\"_am_ip_mon2\":\"127%\",\"_am_ip_tue2\":\"127%\",\"_am_ip_wed2\":\"127%\",\"_am_ip_thu2\":\"127%\",\"_am_ip_fri2\":\"127%\",\"_am_ip_sat2\":\"127%\",\"_am_ip_sun3\":\"127%\",\"_am_ip_mon3\":\"127%\",\"_am_ip_tue3\":\"127%\",\"_am_ip_wed3\":\"127%\",\"_am_ip_thu3\":\"127%\",\"_am_ip_fri3\":\"127%\",\"_am_ip_sat3\":\"127%\",\"_am_ip_sun4\":\"127%\",\"_am_ip_mon4\":\"127%\",\"_am_ip_tue4\":\"127%\",\"_am_ip_wed4\":\"127%\",\"_am_ip_thu4\":\"127%\",\"_am_ip_fri4\":\"127%\",\"_am_ip_sat4\":\"127%\",\"_am_ip_sun5\":\"127%\",\"_am_ip_mon5\":\"127%\",\"_am_ip_tue5\":\"127%\",\"_am_ip_wed5\":\"127%\",\"_am_ip_thu5\":\"127%\",\"_am_ip_fri5\":\"127%\",\"_am_ip_sat5\":\"127%\",\"_am_ip_sun6\":\"127%\",\"_am_ip_mon6\":\"127%\",\"_am_ip_tue6\":\"127%\",\"_am_ip_wed6\":\"127%\",\"_am_ip_thu6\":\"127%\",\"_am_ip_fri6\":\"127%\",\"_am_ip_sat6\":\"127%\",\"_am_ip_sun7\":\"127%\",\"_am_ip_mon7\":\"127%\",\"_am_ip_tue7\":\"127%\",\"_am_ip_wed7\":\"127%\",\"_am_ip_thu7\":\"127%\",\"_am_ip_fri7\":\"127%\",\"_am_ip_sat7\":\"127%\",\"_am_ip_sun8\":\"127%\",\"_am_ip_mon8\":\"127%\",\"_am_ip_tue8\":\"127%\",\"_am_ip_wed8\":\"127%\",\"_am_ip_thu8\":\"127%\",\"_am_ip_fri8\":\"127%\",\"_am_ip_sat8\":\"127%\",\"_am_ip_sun9\":\"127%\",\"_am_ip_mon9\":\"127%\",\"_am_ip_tue9\":\"127%\",\"_am_ip_wed9\":\"127%\",\"_am_ip_thu9\":\"127%\",\"_am_ip_fri9\":\"127%\",\"_am_ip_sat9\":\"127%\",\"_am_ip_sun10\":\"127%\",\"_am_ip_mon10\":\"127%\",\"_am_ip_tue10\":\"127%\",\"_am_ip_wed10\":\"127%\",\"_am_ip_thu10\":\"127%\",\"_am_ip_fri10\":\"127%\",\"_am_ip_sat10\":\"127%\",\"_am_ip_sun11\":\"127%\",\"_am_ip_mon11\":\"127%\",\"_am_ip_tue11\":\"127%\",\"_am_ip_wed11\":\"127%\",\"_am_ip_thu11\":\"127%\",\"_am_ip_fri11\":\"127%\",\"_am_ip_sat11\":\"127%\"}}');
 
 -- --------------------------------------------------------
 
@@ -132,17 +120,14 @@ INSERT INTO `payroll_cards` (`id`, `card_title`, `pay_rate`, `daily_hours`, `hrl
 -- Table structure for table `pays`
 --
 
-DROP TABLE IF EXISTS `pays`;
-CREATE TABLE IF NOT EXISTS `pays` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pays` (
+  `id` int(11) NOT NULL,
   `emp_id` int(11) NOT NULL,
   `monthly_salary` double NOT NULL,
   `calculated_salary` double NOT NULL,
   `salary_type` varchar(20) NOT NULL,
-  `per_hour_amount` double NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `emp_id` (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `per_hour_amount` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pays`
@@ -158,14 +143,11 @@ INSERT INTO `pays` (`id`, `emp_id`, `monthly_salary`, `calculated_salary`, `sala
 -- Table structure for table `positions`
 --
 
-DROP TABLE IF EXISTS `positions`;
-CREATE TABLE IF NOT EXISTS `positions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `positions` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `location` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `location` (`location`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `location` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `positions`
@@ -182,13 +164,11 @@ INSERT INTO `positions` (`id`, `name`, `location`) VALUES
 -- Table structure for table `terminals`
 --
 
-DROP TABLE IF EXISTS `terminals`;
-CREATE TABLE IF NOT EXISTS `terminals` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `terminals` (
+  `id` int(11) NOT NULL,
   `location` text NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `terminals`
@@ -204,13 +184,10 @@ INSERT INTO `terminals` (`id`, `location`, `name`) VALUES
 -- Table structure for table `terminal_links`
 --
 
-DROP TABLE IF EXISTS `terminal_links`;
-CREATE TABLE IF NOT EXISTS `terminal_links` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `position_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `position_id` (`position_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `terminal_links` (
+  `id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `terminal_links`
@@ -227,9 +204,8 @@ INSERT INTO `terminal_links` (`id`, `position_id`) VALUES
 -- Table structure for table `time_clock`
 --
 
-DROP TABLE IF EXISTS `time_clock`;
-CREATE TABLE IF NOT EXISTS `time_clock` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `time_clock` (
+  `id` int(11) NOT NULL,
   `emp_clock_in` datetime NOT NULL,
   `emp_clock_out` datetime NOT NULL,
   `emp_loc` text NOT NULL,
@@ -238,17 +214,140 @@ CREATE TABLE IF NOT EXISTS `time_clock` (
   `clock_out_img` text NOT NULL,
   `emp_clock_out_loc` text NOT NULL,
   `clock_pay` double NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `emp_id` (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=326 DEFAULT CHARSET=latin1;
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `time_clock`
 --
 
 INSERT INTO `time_clock` (`id`, `emp_clock_in`, `emp_clock_out`, `emp_loc`, `emp_id`, `clock_in_img`, `clock_out_img`, `emp_clock_out_loc`, `clock_pay`, `status`) VALUES
-(325, '2019-01-21 10:10:06', '2019-01-21 20:11:46', 'Plot 52, Tipu Block Garden Town, Lahore, Punjab, Pakistan', 63, 'clock_in325.jpeg', 'clock_out325.jpeg', 'Plot 52, Tipu Block Garden Town, Lahore, Punjab, Pakistan', 1320.9722222222, 0);
+(346, '2019-01-23 13:01:55', '2019-01-26 23:00:20', 'Plot 52, Tipu Block Garden Town, Lahore, Punjab, Pakistan', 63, 'clock_in346.jpeg', 'clock_out346.jpeg', 'Plot 52, Tipu Block Garden Town, Lahore, Punjab, Pakistan', 11056.701388889, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clock_day_counter`
+--
+ALTER TABLE `clock_day_counter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `clock_index` (`clock_index`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_addr` (`email_addr`),
+  ADD KEY `payroll_card` (`payroll_card`);
+
+--
+-- Indexes for table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `payroll_cards`
+--
+ALTER TABLE `payroll_cards`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pays`
+--
+ALTER TABLE `pays`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `positions`
+--
+ALTER TABLE `positions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location` (`location`);
+
+--
+-- Indexes for table `terminals`
+--
+ALTER TABLE `terminals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `terminal_links`
+--
+ALTER TABLE `terminal_links`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `position_id` (`position_id`);
+
+--
+-- Indexes for table `time_clock`
+--
+ALTER TABLE `time_clock`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `emp_id` (`emp_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clock_day_counter`
+--
+ALTER TABLE `clock_day_counter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+
+--
+-- AUTO_INCREMENT for table `leaves`
+--
+ALTER TABLE `leaves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `payroll_cards`
+--
+ALTER TABLE `payroll_cards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `pays`
+--
+ALTER TABLE `pays`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `positions`
+--
+ALTER TABLE `positions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `terminals`
+--
+ALTER TABLE `terminals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `terminal_links`
+--
+ALTER TABLE `terminal_links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `time_clock`
+--
+ALTER TABLE `time_clock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=347;
 
 --
 -- Constraints for dumped tables
